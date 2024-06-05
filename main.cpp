77944b4e6fe7e73156c6e4c3b22c3f826f3ae847
Dimitris Platis <dimitris.platis@neat.no>
2024-06-05 10:34:15 +0300

    Add enum value wrapper example

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 #include "switcheroo/switcheroo.h"
 
+// Enum types as classes
 struct Red {
     std::string red{"red"};
 };
@@ -16,12 +17,36 @@ struct Blue {
     std::string blue{"blue"};
 };
 
+// Enum types wrapped in a template class
+
+enum class MonthType {
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+};
+
+template<MonthType T>
+struct Month {
+    static constexpr MonthType value = T;
+};
+
 int main()
 {
     using namespace std::string_literals;
     using namespace switcheroo;
 
-    const std::variant<std::monostate, Red, Green, Blue> color{Green{}};
+    // Enum types as classes example
+    using Color = std::variant<std::monostate, Red, Green, Blue>;
+    const Color color{Green{}};
 
     const auto result
         = match(color)
@@ -38,6 +63,33 @@ int main()
                              .otherwise([](auto) { return -1; })
                              .run();
     std::cout << "Result2: " << result2 << '\n'; // Result2: -1
+
+    // Enum types wrapped in a template class example
+    using MonthT = std::variant<Month<MonthType::January>,
+                                Month<MonthType::February>,
+                                Month<MonthType::March>,
+                                Month<MonthType::April>,
+                                Month<MonthType::May>,
+                                Month<MonthType::June>,
+                                Month<MonthType::July>,
+                                Month<MonthType::August>,
+                                Month<MonthType::September>,
+                                Month<MonthType::October>,
+                                Month<MonthType::November>,
+                                Month<MonthType::December>>;
+
+    const MonthT month{Month<MonthType::February>{}};
+
+    const auto goodWeather
+        = match(month)
+              .when<Month<MonthType::June>>([](auto) { return true; })
+              .when<Month<MonthType::July>>([](auto) { return true; })
+              .when<Month<MonthType::August>>([](auto) { return true; })
+              .otherwise([](auto) { return false; })
+              .run();
+
+    std::cout << "Good weather: " << std::boolalpha << goodWeather
+              << '\n'; // Good weather: false
 
     return 0;
 }
